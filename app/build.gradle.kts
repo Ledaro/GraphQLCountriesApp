20c5070978b7ae79aa6d159d6b102638909498cd
Ledaro <fzelaskowski@wp.pl>
2023-03-05 20:44:20 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("dagger.hilt.android.plugin")
+    id("com.apollographql.apollo3").version("3.7.4")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.example.graphqlcountriesapp"
+        minSdk = 27
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Apollo
+    implementation("com.apollographql.apollo3:apollo-runtime:3.7.4")
+
+    //Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.42")
+    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.graphqlcountriesapp")
+    }
+}
